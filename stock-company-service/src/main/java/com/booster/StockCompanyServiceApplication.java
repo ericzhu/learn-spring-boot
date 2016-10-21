d36ced8b353b16ab35be543a50896e4a5029f4cc
Eric Zhu <eric.zhu@esignlive.com>
2016-10-21 10:03:22 -0400

    add .gitignore file

--- stock-company-service/src/main/java/com/booster/StockCompanyServiceApplication.java
+++ stock-company-service/src/main/java/com/booster/StockCompanyServiceApplication.java
@@ -15,19 +15,21 @@ import com.booster.repository.CompanyRepository;
 @SpringBootApplication
 public class StockCompanyServiceApplication {
 
-    public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(StockCompanyServiceApplication.class, args);
-        CompanyRepository companyRepository = context.getBean(CompanyRepository.class);
-        Stream.of("GE", "IBM", "GOOGLE", "TESLA").forEach(c -> companyRepository.save(new Company(c)));
-        
-        companyRepository.findAll().forEach(c -> System.out.println(c.getName()));
-    }
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(StockCompanyServiceApplication.class, args);
+		CompanyRepository companyRepository = context.getBean(CompanyRepository.class);
+		Stream.of("GE", "IBM", "GOOGLE", "TESLA", "MSFT", "PYPL", "INTC", "AAPL", "CSCO", "SIRI", "MU", "ALKS", "QQQ",
+				"QCOM", "HBAN", "XIV", "EBAY", "FB", "CMCSA", "GILD", "AMAT", "AAL", "MDLZ", "NXPI")
+				.forEach(c -> companyRepository.save(new Company(c)));
+
+		companyRepository.findAll().forEach(c -> System.out.println(c.getName()));
+	}
 }
 
 @Configuration
 class RepositoryConfig extends RepositoryRestMvcConfiguration {
-    @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Company.class);
-    }
+	@Override
+	protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Company.class);
+	}
 }
