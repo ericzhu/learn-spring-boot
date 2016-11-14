407c5140900a1f2f374f21e943fcc5b5471e71d7
Eric Zhu <eric.zhu@esignlive.com>
2016-11-14 13:26:32 -0500

    add form for adding images

--- demo-estore/src/main/java/com/booster/ecom/web/ui/controller/ImageController.java
+++ demo-estore/src/main/java/com/booster/ecom/web/ui/controller/ImageController.java
@@ -1,10 +1,6 @@
 package com.booster.ecom.web.ui.controller;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -12,17 +8,18 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.booster.ecom.service.ImageService;
 
-@RestController
+@Controller
 public class ImageController {
 
     public static final String BASE_PATH = "/images";
@@ -44,19 +41,16 @@ public class ImageController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = BASE_PATH)
-    @ResponseBody
-    public ResponseEntity<?> createFile(@RequestParam("file") MultipartFile file, HttpServletRequest servletRequest) {
+    public String createFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 
         try {
             imageService.createImage(file);
-
-            final URI locationUri = new URI(servletRequest.getRequestURL().toString() + "/").resolve(file.getOriginalFilename() + "/raw");
-
-            return ResponseEntity.created(locationUri).body("successfully upload " + file.getOriginalFilename());
+            redirectAttributes.addFlashAttribute("flash.message", "successfully uploaded " + file.getOriginalFilename());
         }
-        catch (IOException | URISyntaxException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("failed to upload " + file.getOriginalFilename() + " => " + e.getMessage());
+        catch (Exception e) {
+            redirectAttributes.addFlashAttribute("flash.message", "failed to upload " + file.getOriginalFilename());
         }
+        return "redirect:/";
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = BASE_PATH + "/" + FILE_NAME)
