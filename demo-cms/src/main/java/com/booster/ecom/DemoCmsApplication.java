621ed7da7be3595f0958aa4fffeef5c2205b2089
Eric Zhu <eric.zhuxiaoyu@gmail.com>
2016-11-17 22:44:36 -0500

    add cms project

---
+++ demo-cms/src/main/java/com/booster/ecom/DemoCmsApplication.java
@@ -0,0 +1,12 @@
+package com.booster.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCmsApplication.class, args);
+	}
+}
---
+++ demo-cms/src/test/java/com/booster/ecom/DemoCmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.booster.ecom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoCmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
