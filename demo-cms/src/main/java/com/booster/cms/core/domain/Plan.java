952f65b43015d966eac0fee3a34cb0cf59b35ca3
Eric Zhu <eric.zhu@esignlive.com>
2016-11-22 09:39:59 -0500

    add security domain model

---
+++ demo-cms/src/main/java/com/booster/cms/core/domain/Plan.java
@@ -0,0 +1,39 @@
+package com.booster.cms.core.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Plan implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public Plan() {}
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long   id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
